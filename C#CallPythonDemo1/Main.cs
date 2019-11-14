cf480570dfc6a21204275cb5f3a9c329a59150be
萌虎 <cxm2972@163.com>
2019-11-14 11:50:35 +0800

    提交c#2Python

---
+++ C#CallPythonDemo1/Main.cs
@@ -0,0 +1,20 @@
+﻿using IronPython.Hosting;
+using Microsoft.Scripting.Hosting;
+using System;
+
+namespace CSharpCallPython
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ScriptEngine pyEngine = Python.CreateEngine();//创建Python解释器对象
+            dynamic py = pyEngine.ExecuteFile(@"test.py");//读取脚本文件
+            int[] array = new int[9] { 9, 3, 5, 7, 2, 1, 3, 6, 8 };
+            string reStr = py.main(array);//调用脚本文件中对应的函数
+            Console.WriteLine(reStr);
+
+            Console.ReadKey();
+        }
+    }
+}
